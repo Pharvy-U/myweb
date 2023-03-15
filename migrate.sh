95dbe4fe2d87fea0d6e684dc0bcd69c432bda6c2
Pharvy-U <ukasoanyafavour6@gmail.com>
2023-03-15 02:27:54 +0100

    Enabled a postgres db and automatic migration on app startup

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,4 @@
 SUPERUSER_EMAIL = ${DJANGO_SUPERUSER_EMAIL:-"ukasoanyafavour6@gmail.com"}
 cd /app/
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuuperuser --email $SUPERUSER_EMAIL --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email "${SUPERUSER_EMAIL}" --noinput || true
