bfdc5f0c98ce6691d85e8aad3b9e8e7e866bc2df
Pharvy-U <ukasoanyafavour6@gmail.com>
2023-03-17 19:12:21 +0100

    Applied the database fix, not that it matters though

--- myweb/settings.py
+++ myweb/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     }
 }
 
-DB_NAME = os.environ.get('POSTGRES_DATABASE')
-DB_USERNAME = os.environ.get('POSTGRES_USERNAME')
+DB_NAME = os.environ.get('POSTGRES_DB')
+DB_USERNAME = os.environ.get('POSTGRES_USER')
 DB_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 DB_HOST = os.environ.get('POSTGRES_HOST')
 DB_PORT = os.environ.get('POSTGRES_PORT')
