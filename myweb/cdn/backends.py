f6aa8e76f3522a47064d9d158a44b9fff344f01c
Favour <ukasoanya@systemspecs.com.ng>
2023-05-10 22:54:59 +0100

    Fixed bucket ACL error

--- myweb/cdn/backends.py
+++ myweb/cdn/backends.py
@@ -1,10 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
 class StaticRootS3BotoStorage(S3Boto3Storage):
     location = "static"
-    default_acl = 'public-read'
 
 class MediaRootS3BotoStorage(S3Boto3Storage):
     location = "media"
-    default_acl = 'public-read'
     file_overwrite = False
