20050e049bd8c3eaf2fd5577b7b3f9260372c6b9
Favour <ukasoanya@systemspecs.com.ng>
2024-12-12 13:07:19 +0100

    Serve static files 2

--- myweb/settings.py
+++ myweb/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
