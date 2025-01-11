fa37ec867c75a8f293dc277345f7d48a06bc77a1
Favour <ukasoanya@systemspecs.com.ng>
2025-01-11 01:30:53 +0100

    Switch dummy email to smtp

--- myweb/settings.py
+++ myweb/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
